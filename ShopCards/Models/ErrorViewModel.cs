52d84df737026bf98e10dab79229247cbbd504fe
Szymon Bywalec <simonbws6@gmail.com>
2022-08-22 22:18:34 +0200

    Dodaj pliki projektów.

---
+++ ShopCards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopCards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
