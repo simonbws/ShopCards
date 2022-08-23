7fe9d2244c38ca143f224e032b1fd7c2534a1bef
Szymon Bywalec <simonbws6@gmail.com>
2022-08-23 19:24:12 +0200

    Added Tag Helpers

---
+++ ShopCards/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopCards.Database;
+
+namespace ShopCards.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
